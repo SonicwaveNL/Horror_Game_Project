1c09244c12e1991af4bd4abfbbd2c24257c1c521
DuurAlblas <duur.alblas@gmail.com>
2020-01-15 11:17:25 +0100

    Changed where constructors are

--- Core/Headers/support.hpp
+++ Core/Headers/support.hpp
@@ -6,7 +6,7 @@
 
 ///@file
 ///\brief
-///Action class to assign a action (work) to a event (condition).
+/// Action class to assign a action (work) to a event (condition).
 ///\details
 /*The Action class is used to do work in case a condition is met.
 There are several constructors to facilitate different base cases.*/
@@ -17,43 +17,52 @@ class Action {
 
   public:
     ///\brief
-    ///Constructor that expects a conditiona and the work.
+    /// Constructor that expects a conditiona and the work.
     ///@param condition
-    /*First parameter condition of type std::function<bool()>, 
-    store a lambda with return type bool or std::function<bool()> 
+    /*First parameter condition of type std::function<bool()>,
+    store a lambda with return type bool or std::function<bool()>
     which will be the condition for the work to happen.*/
     ///@param work
     /*Second parameter work of type std::function<void()>,
-    store a lambda with void return type or std::function<void()> 
+    store a lambda with void return type or std::function<void()>
     when the condition is fullfilled the stored "work" will be done.*/
-    Action(std::function<bool()> condition, std::function<void()> work);
+    Action(std::function<bool()> condition, std::function<void()> work)
+      : condition(condition), 
+     work(work) {};
     ///\brief
-    ///Constructor that expects a sf::Keyboard::Key as condition.
+    /// Constructor that expects a sf::Keyboard::Key as condition.
     ///@param key
-    /*sf::Keyboard::Key the key that has to be pressed for the work to be done.*/
+    /*sf::Keyboard::Key the key that has to be pressed for the work to be
+     * done.*/
     ///@param work
     /*Second parameter work of type std::function<void()>,
-    store a lambda with void return type or std::function<void()> 
+    store a lambda with void return type or std::function<void()>
     when the condition is fullfilled the stored "work" will be done.*/
-    Action(sf::Keyboard::Key key, std::function<void()> work);
+    Action(sf::Keyboard::Key key, std::function<void()> work)
+      : condition([key]() -> bool { return sf::Keyboard::isKeyPressed(key); }),
+      work(work) {};
     ///\brief
-    ///Constructor that expects a sf::Mouse::Button as condition.
+    /// Constructor that expects a sf::Mouse::Button as condition.
     ///@param button
-    /*sf::Mouse::Button the mouse button that has to be pressed for the work to be done.*/
+    /*sf::Mouse::Button the mouse button that has to be pressed for the work to
+     * be done.*/
     ///@param work
     /*Second parameter work of type std::function<void()>,
-    store a lambda with void return type or std::function<void()> 
+    store a lambda with void return type or std::function<void()>
     when the condition is fullfilled the stored "work" will be done.*/
-    Action(sf::Mouse::Button button, std::function<void()> work);
+    Action(sf::Mouse::Button button, std::function<void()> work) 
+      : condition([button]() -> bool { return sf::Mouse::isButtonPressed(button); }),
+      work(work) {};
     ///\brief
-    ///Constructor without a condition.
+    /// Constructor without a condition.
     ///@param work
     /*Second parameter work of type std::function<void()>,
-    store a lambda with void return type or std::function<void()> 
+    store a lambda with void return type or std::function<void()>
     when the Action() is used the stored "work" will be done.*/
-    Action(std::function<void()> work);
+    Action(std::function<void()> work)
+      : condition([]() -> bool { return true; }), work(work) {};
     ///\brief
-    ///Used to "call" the class.
+    /// Used to "call" the class.
     ///\details
     /*When the class is "called" the condition will be checked,
     if the condition is true the "work" will be done.*/
@@ -61,22 +70,19 @@ class Action {
 };
 
 ///\brief
-///Enum class that contains input types.
-enum class inputType {
-    keyboard,
-    controller
-};
+/// Enum class that contains input types.
+enum class inputType { keyboard, controller };
 
 ///\brief
-///Enum class that contains keywords representing actions.
-enum class actionKeyword{
+/// Enum class that contains keywords representing actions.
+enum class actionKeyword {
     escape,
     select,
     left,
     up,
     right,
     down,
-    //Reserved action keywords, usage depends on game state.
+    // Reserved action keywords, usage depends on game state.
     action1,
     action2,
     action3,
--- Core/Sources/support.cpp
+++ Core/Sources/support.cpp
@@ -1,20 +1,5 @@
 #include "../Headers/support.hpp"
 
-Action::Action(std::function<bool()> condition, std::function<void()> work)
-    : condition(condition), work(work) {}
-
-Action::Action(sf::Keyboard::Key key, std::function<void()> work)
-    : condition([key]() -> bool { return sf::Keyboard::isKeyPressed(key); }),
-      work(work) {}
-
-Action::Action(sf::Mouse::Button button, std::function<void()> work)
-    : condition(
-          [button]() -> bool { return sf::Mouse::isButtonPressed(button); }),
-      work(work) {}
-
-Action::Action(std::function<void()> work)
-    : condition([]() -> bool { return true; }), work(work) {}
-
 void Action::operator()() {
     if (condition()) {
         work();
