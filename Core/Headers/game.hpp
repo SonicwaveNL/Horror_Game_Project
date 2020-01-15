4feee30735fd95210eac4534021098f4feb049e3
DuurAlblas <duur.alblas@gmail.com>
2020-01-15 14:09:23 +0100

    Added the door to the drawables

--- Core/Headers/game.hpp
+++ Core/Headers/game.hpp
@@ -34,11 +34,12 @@ public:
     ///\details
     /*Creates the 4 walls, a door and a player*/
     Game():{
-        drawables.push_back(std::make_unique<Player>(sf::Vector2f(50.f,50.f),sf::Vector2f(25.f,25.f)    ,drawables));
-        drawables.push_back(std::make_unique<Wall>(sf::Vector2f(0.f,0.f)    ,sf::Vector2f(20.f,1080.f)  ,drawables));
-        drawables.push_back(std::make_unique<Wall>(sf::Vector2f(0.f,0.f)    ,sf::Vector2f(1920.f,20.f)  ,drawables));
-        drawables.push_back(std::make_unique<Wall>(sf::Vector2f(1900.f,0.f) ,sf::Vector2f(20.f,1080.f)  ,drawables));
-        drawables.push_back(std::make_unique<Wall>(sf::Vector2f(0.f,1060.f) ,sf::Vector2f(1920.f,20.f)  ,drawables));
+        drawables.push_back(std::make_unique<Player>(sf::Vector2f(50.f,50.f)    ,sf::Vector2f(25.f,25.f)    ,drawables));
+        drawables.push_back(std::make_unique<Wall>(sf::Vector2f(0.f,0.f)        ,sf::Vector2f(20.f,1080.f)  ,drawables));
+        drawables.push_back(std::make_unique<Wall>(sf::Vector2f(0.f,0.f)        ,sf::Vector2f(1920.f,20.f)  ,drawables));
+        drawables.push_back(std::make_unique<Wall>(sf::Vector2f(1900.f,0.f)     ,sf::Vector2f(20.f,1080.f)  ,drawables));
+        drawables.push_back(std::make_unique<Wall>(sf::Vector2f(0.f,1060.f)     ,sf::Vector2f(1920.f,20.f)  ,drawables));
+        drawables.push_back(std::make_unique<door>(sf::Vector2f(1895.f,500.f)   ,sf::Vector2f(10.f,80.f)    ,drawables));
         player = *drawables[0];
     };
     ///\brief
