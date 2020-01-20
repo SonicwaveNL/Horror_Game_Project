c67a5cffa0c680853abf7ae689d649305317383b
Vincent van Setten <vvampus@gmail.com>
2020-01-20 15:20:38 +0100

    still working

---
+++ Core/Sources/gridCell.cpp
@@ -0,0 +1,21 @@
+#include <gridCell.hpp>
+
+void GridCell::moveIfPossible(sf::Vector2f direction){
+    return;
+}
+
+void GridCell::move(sf::Vector2f direction){
+    return;    
+}
+
+bool GridCell::isColliding(IObject & obj){
+
+}
+
+void GridCell::interact(IObject & obj){
+
+}
+
+void GridCell::setType(std::string type){
+    type=type;
+}
