df7dd5b2b700fda128d9c1c481ef7c54fd0644c8
OscarKromhout <oscarkromhout@gmail.com>
2020-01-22 10:36:28 +0100

    added doxygen to the A.I

--- Core/Headers/game.hpp
+++ Core/Headers/game.hpp
@@ -71,11 +71,30 @@ class Game {
 
   std::vector<std::vector<GridCell>> createGrid(sf::Vector2u windowSize);
   std::array<int, 2> findShapeFromMouse(sf::Vector2f mousePos);
+  
+  ///\brief
+  ///Small Algorithm used by the Monster
+  ///\details
+  /*This Algorithm finds the next step/direction to go to. He does so by checking the value in all the GridCells next to the
+  Monster, comparing which one is the fastest and then returning a vector which holds the direction the monster should go to.
+  See the details for reversedBFSPathAlgorithm for further details on this subject.*/
+  ///@return sf::Vector2f, containing the direction the Monster should move to. 
+  sf::Vector2f findShortestStep();
+  
+  ///\brief
+  ///Reversed Breath First Search Algorithm.
+  ///\details
+  /*This Algorithm is an implementation of the BFS algorithm. And can only be used for this game. It basically finds the shortest
+  path from the player to the Monster (hence "reversed") so the Monster can then use the findShortestStep() algorithm to see
+  which direction is the fastest to the player. The algorithm itterates over all cells in the grid and checks which cells are walkable
+  and which ones aren't. It simultaneously checks on which cell the player is and saves that. It then checks all walkable cells from the 
+  player to the monster using a queue and fills each cell with the distance from the player to that respective cell (the value). The
+  algorithm does so until it finds the cell containing the monster and then ends. From that moment forward, all
+  cells from the monster to the player contain a number. The monster can then use the findShortestStep() algorithm to check which
+  cells around him have to lowest value, and thus which cell contains the shortest path to the player. */
+  void reversedBFSPathAlgorithm();
   ///\brief
   /// Runs the game demo
-
-  sf::Vector2f findShortestStep();
-  void reversedBFSPathAlgorithm();
   void run();
 };
 
