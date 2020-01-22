b33ea7c2ceca3c541a1f34fcb7d4cad33a8e26c6
MHovenkamp <maaike.hovenkamp@gmail.com>
2020-01-22 10:00:24 +0100

    filefactory in apparte hpp en cpp

--- Core/Headers/fileFactory.hpp
+++ Core/Headers/fileFactory.hpp
@@ -6,8 +6,12 @@
 
 #include <support.hpp>
 #include <gridCell.hpp>
+#include <wall.hpp>
+#include <player.hpp>
+#include <monster.hpp>
+#include <door.hpp>
 
-class fileFactory{
+class FileFactory{
 private:
     const struct { objectType itemType; std::string writeAble; }
         types[8]{
@@ -22,85 +26,11 @@ private:
         };
 public:
 
-    void writeToFile(std::vector<std::vector<GridCell>> & matrix, std::string fileName ){
-        std::ofstream file;
-        file.open(fileName); //@TODO exception maken en testen of hij al bestaat
-        for( auto & row: matrix ){
-            for( auto & item: row ){
-                auto itemSoort = item.getCellType();
-                for( auto const & soort: types ){
-                    if( itemSoort == soort.itemType ){
-                        file << soort.writeAble;
-                        file << "(" << item.getPosition().x << "," << item.getPosition().y << ")\n";
-                    }
-                }
-            }
-        }
-    }
+    void writeToFile(std::vector<std::vector<GridCell>> & matrix, std::string fileName );
 
-    void loadMatrixFromFile(std::vector<std::vector<GridCell>> & matrix, std::string fileName){
-        std::ifstream file;
-        file.open(fileName); //@TODO exception maken
-        std::string tmpString;
-        int xCoordinate, yCoordinate;
-        char tmpChar;
-        while(!EOF){
-            file >> tmpString;
+    void loadMatrixFromFile(std::vector<std::vector<GridCell>> & matrix, std::string fileName);
 
-            if( !(file >> tmpChar) ){ return; } //spatie
-
-            if( !(file >> tmpChar) ){ return; }
-            if( !(tmpChar != '(') ){ return; }
-
-            if( !(file >> xCoordinate) ){ return; }
-
-            if( !(file >> tmpChar) ){ return; }
-            if( !(tmpChar != ',' ) ){ return; }
-
-            if( !(file >> yCoordinate) ){ return; }
-
-            if( !(file >> tmpChar) ){ return; }
-            if( !(tmpChar != ')') ){ return; }
-
-            for( auto & item : types){
-                if( tmpString == item.writeAble){
-                    matrix[xCoordinate/20][yCoordinate/20].setCellType( item.itemType );
-                    matrix[xCoordinate/20][yCoordinate/20].setPosition( sf::Vector2f( (xCoordinate), yCoordinate/20 ));
-                }
-            }
-        }
-
-        std::vector<std::shared_ptr<IObject>> objectsToDrawables( std::vector<std::vector<GridCell>> & matrix ){
-            std::vector<std::shared_ptr<IObject>> returnVector;
-            objectType soort;
-            int x = 0;
-            int y = 0;
-            for( auto & row: matrix ){
-                for( auto & item: row ){
-                    soort = item.getCelType();
-                    if( soort == objectType::Abstract){
-                        // throw exception
-                    } else if( soort == objectType::GridCell ){
-                        // throw exception
-                    } else if( soort == objectType::Floor ){
-                        // empty
-                    } else if( soort == objectType::Wall ){
-                        std::make_unique<Wall>();
-                    } else if( soort == objectType::Switch ){
-                        std::make_unique<>();
-                    } else if( soort == objectType::Door ){
-                        std::make_unique<>();
-                    } else if( soort == objectType::Player ){
-                        std::make_unique<>();
-                    } else if( soort == objectType::Monster ){
-
-                    }
-                    y++
-                }
-                x++
-            }
-        }
-    }
+    std::vector<std::shared_ptr<IObject>> objectsToDrawables( std::vector<std::vector<GridCell>> & matrix );
 };
 
 #endif
---
+++ Core/Sources/fileFactory.cpp
@@ -0,0 +1,80 @@
+#include "fileFactory.hpp"
+
+void FileFactory::writeToFile(std::vector<std::vector<GridCell>> & matrix, std::string fileName ){
+    std::ofstream file;
+    file.open(fileName); //@TODO exception maken en testen of hij al bestaat
+    for( auto & row: matrix ){
+        for( auto & item: row ){
+            auto itemSoort = item.getCellType();
+            for( auto const & soort: types ){
+                if( itemSoort == soort.itemType ){
+                    file << soort.writeAble;
+                    file << "(" << item.getPosition().x << "," << item.getPosition().y << ")\n";
+                }
+            }
+        }
+    }
+}
+
+void FileFactory::loadMatrixFromFile(std::vector<std::vector<GridCell>> & matrix, std::string fileName){
+    std::ifstream file;
+    file.open(fileName); //@TODO exception maken
+    std::string tmpString;
+    int xCoordinate, yCoordinate;
+    char tmpChar;
+    while(!EOF){
+        file >> tmpString;
+
+        if( !(file >> tmpChar) ){ return; } //spatie
+
+        if( !(file >> tmpChar) ){ return; }
+        if( !(tmpChar != '(') ){ return; }
+
+        if( !(file >> xCoordinate) ){ return; }
+
+        if( !(file >> tmpChar) ){ return; }
+        if( !(tmpChar != ',' ) ){ return; }
+
+        if( !(file >> yCoordinate) ){ return; }
+
+        if( !(file >> tmpChar) ){ return; }
+        if( !(tmpChar != ')') ){ return; }
+
+        for( auto & item : types){
+            if( tmpString == item.writeAble){
+                matrix[xCoordinate/20][yCoordinate/20].setCellType( item.itemType );
+                matrix[xCoordinate/20][yCoordinate/20].setPosition( sf::Vector2f( xCoordinate, yCoordinate ));
+            }
+        }
+    }
+}
+
+std::vector<std::shared_ptr<IObject>> FileFactory::objectsToDrawables( std::vector<std::vector<GridCell>> & matrix ){
+    std::vector<std::shared_ptr<IObject>> returnVector;
+    objectType soort;
+    for( auto & row: matrix ){
+        for( auto & item: row ){
+            soort = item.getCellType();
+            if( soort == objectType::Abstract){
+                // throw exception
+                continue;
+            } else if( soort == objectType::GridCell ){
+                // throw exception
+                continue;
+            } else if( soort == objectType::Floor ){
+                continue;
+            } else if( soort == objectType::Wall ){
+                auto pointer = std::make_unique<Wall>( item.getPosition(), returnVector );
+            } else if( soort == objectType::Switch ){
+                auto pointer = std::make_unique<Switch>( item.getPosition(), returnVector );
+            } else if( soort == objectType::Door ){
+                auto pointer = std::make_unique<Door>( item.getPosition(), returnVector );
+            } else if( soort == objectType::Player ){
+                auto pointer = std::make_unique<Player>( item.getPosition(), returnvector );
+            } else if( soort == objectType::Monster ){
+                auto pointer = std::make_unique<Monster>( item.getPosition(), returnvector );
+            }
+        }
+    }
+    returnVector.push_back( pointer );
+}
