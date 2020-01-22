33871dd37bedd003569cc8b1c26da049bf89cd84
sonicwavenl <justin.van.ziel@gmail.com>
2020-01-22 10:14:05 +0100

    Added A.I. to game class

--- Core/Headers/game.hpp
+++ Core/Headers/game.hpp
@@ -2,79 +2,81 @@
 #define GAME_HPP
 
 #include <SFML/Graphics.hpp>
-#include <memory>
-#include <vector>
-#include <queue>
-#include <iObject.hpp>
-#include <gridCell.hpp>
-#include <support.hpp>
 #include <action.hpp>
-#include <player.hpp>
+#include <gridCell.hpp>
+#include <iObject.hpp>
+#include <memory>
 #include <monster.hpp>
+#include <player.hpp>
+#include <queue>
+#include <support.hpp>
+#include <vector>
 
 ///@file
 
 ///\brief
 /// Game class to present demo
 class Game {
-  private:
-    sf::RenderWindow window{sf::VideoMode{1920, 1080}, "Booh - The game",
-                            sf::Style::Fullscreen};
-    std::vector<std::shared_ptr<IObject>> drawables;
-    std::shared_ptr<Player> player;
-    std::shared_ptr<Monster> monster;
-    std::vector<std::vector<GridCell>> grid;
-    std::string cellType = "Floor";
+ private:
+  sf::RenderWindow window{sf::VideoMode{1920, 1080}, "Booh - The game",
+                          sf::Style::Fullscreen};
+  std::vector<std::shared_ptr<IObject>> drawables;
+  std::shared_ptr<Player> player;
+  std::shared_ptr<Monster> monster;
+  std::vector<std::vector<GridCell>> grid;
+  std::string cellType = "Floor";
 
-    Action playingActions[4] = {
-        Action(actionKeyword::up,
-               [=]() { player->moveIfPossible(sf::Vector2f(0.f, -1.f)); }),
-        Action(actionKeyword::down,
-               [=]() { player->moveIfPossible(sf::Vector2f(0.f, 1.f)); }),
-        Action(actionKeyword::left,
-               [=]() { player->moveIfPossible(sf::Vector2f(-1.f, 0.f)); }),
-        Action(actionKeyword::right,
-               [=]() { player->moveIfPossible(sf::Vector2f(1.f, 0.f)); })};
+  Action playingActions[4] = {
+      Action(actionKeyword::up,
+             [=]() { player->moveIfPossible(sf::Vector2f(0.f, -1.f)); }),
+      Action(actionKeyword::down,
+             [=]() { player->moveIfPossible(sf::Vector2f(0.f, 1.f)); }),
+      Action(actionKeyword::left,
+             [=]() { player->moveIfPossible(sf::Vector2f(-1.f, 0.f)); }),
+      Action(actionKeyword::right,
+             [=]() { player->moveIfPossible(sf::Vector2f(1.f, 0.f)); })};
 
-    Action editorActions[6] = {
-        Action(sf::Keyboard::Num1, [=]() { cellType = "Wall"; }),
-        Action(sf::Keyboard::Num2, [=]() { cellType = "Switch"; }),
-        Action(sf::Keyboard::Num3, [=]() { cellType = "Door"; }),
-        Action(sf::Keyboard::Num4, [=]() { cellType = "Player"; }),
-        Action(sf::Keyboard::Num5, [=]() { cellType = "Monster"; }),
-        Action(sf::Mouse::Button::Left, [&]() {
-            sf::Vector2f mousePos =
-                window.mapPixelToCoords(sf::Mouse::getPosition(window));
-            int index[2] = {int(mousePos.x) % 20, int(mousePos.y) % 20};
-            std::cout << "0 :  " << index[0] << "| 1 : " << index[1] << std::endl;
-            std::cout << grid[index[0]][index[1]].getCellType() << std::endl;
-            grid[index[0]][index[1]].setCellType(cellType);
-        })};
+  Action editorActions[6] = {
+      Action(sf::Keyboard::Num1, [=]() { cellType = "Wall"; }),
+      Action(sf::Keyboard::Num2, [=]() { cellType = "Switch"; }),
+      Action(sf::Keyboard::Num3, [=]() { cellType = "Door"; }),
+      Action(sf::Keyboard::Num4, [=]() { cellType = "Player"; }),
+      Action(sf::Keyboard::Num5, [=]() { cellType = "Monster"; }),
+      Action(sf::Mouse::Button::Left, [&]() {
+        sf::Vector2f mousePos =
+            window.mapPixelToCoords(sf::Mouse::getPosition(window));
+        int index[2] = {int(mousePos.x) % 20, int(mousePos.y) % 20};
+        std::cout << "0 :  " << index[0] << "| 1 : " << index[1] << std::endl;
+        std::cout << grid[index[0]][index[1]].getCellType() << std::endl;
+        grid[index[0]][index[1]].setCellType(cellType);
+      })};
 
-  public:
-    ///\brief
-    /// Game constructor
-    ///\details
-    /*Creates the 4 walls, a door and a player*/
-    Game() {
-        drawables.push_back(
-            std::make_shared<Player>(sf::Vector2f(50.f, 50.f), drawables));
-        drawables.push_back(
-            std::make_shared<Monster>( sf::Vector2f(50.f, 50.f), drawables));
-        player = std::static_pointer_cast<Player>(drawables[0]);
-        monster = std::static_pointer_cast<Monster>(drawables[1]);
-        grid = createGrid(window.getSize());
-        std::cout << "grid size x : " << grid.size() << std::endl;
-    };
+ public:
+  ///\brief
+  /// Game constructor
+  ///\details
+  /*Creates the 4 walls, a door and a player*/
+  Game() {
+    drawables.push_back(
+        std::make_shared<Player>(sf::Vector2f(50.f, 50.f), drawables));
+    drawables.push_back(
+        std::make_shared<Monster>(sf::Vector2f(50.f, 50.f), drawables));
+    player = std::static_pointer_cast<Player>(drawables[0]);
+    monster = std::static_pointer_cast<Monster>(drawables[1]);
+    grid = createGrid(window.getSize());
+    std::cout << "grid size x : " << grid.size() << std::endl;
 
-    std::vector<std::vector<GridCell>> createGrid(sf::Vector2u windowSize);
-    std::array<int, 2> findShapeFromMouse(sf::Vector2f mousePos);
-    ///\brief
-    /// Runs the game demo
-    void run();
+    reversedBFSPathAlgorithm();
+  };
 
-    sf::Vector2f findShortestStep();
-    void reversedBFSPathAlgorithm();
+  std::vector<std::vector<GridCell>> createGrid(sf::Vector2u windowSize);
+  std::array<int, 2> findShapeFromMouse(sf::Vector2f mousePos);
+  ///\brief
+  /// Runs the game demo
+
+  sf::Vector2f findShortestStep();
+  void reversedBFSPathAlgorithm();
+  void run();
 };
 
 #endif
--- Core/Sources/game.cpp
+++ Core/Sources/game.cpp
@@ -1,195 +1,198 @@
 #include <../Headers/game.hpp>
 
-std::vector< std::vector< GridCell >> Game::createGrid( sf::Vector2u windowSize ){
-    unsigned int amountOfColumn = windowSize.x / 20;
-    unsigned int amountRectRow = windowSize.y / 20;
-    unsigned int amountOfRect = amountRectRow * amountOfColumn;
-    float x = 0;
-    float y = 0;
+std::vector<std::vector<GridCell>> Game::createGrid(sf::Vector2u windowSize) {
+  unsigned int amountOfColumn = windowSize.x / 20;
+  unsigned int amountRectRow = windowSize.y / 20;
+  unsigned int amountOfRect = amountRectRow * amountOfColumn;
+  float x = 0;
+  float y = 0;
 
-    std::vector< std::vector< GridCell >> shapeMatrix;
+  std::vector<std::vector<GridCell>> shapeMatrix;
 
-    for (size_t i = 0; i < amountOfColumn; i++){
-        shapeMatrix.push_back(std::vector<GridCell>());
-        int posX = (int)x / 20;
-        for (size_t j = 0; j < amountRectRow; j++){
-            int posY = (int)y / 20;
-            y+=20;
-            shapeMatrix[posX].push_back(GridCell((sf::Vector2f(x,y)), drawables));
-        }
-        x+=20;
-        y=0;
+  for (size_t i = 0; i < amountOfColumn; i++) {
+    shapeMatrix.push_back(std::vector<GridCell>());
+    int posX = (int)x / 20;
+    for (size_t j = 0; j < amountRectRow; j++) {
+      int posY = (int)y / 20;
+      y += 20;
+      shapeMatrix[posX].push_back(GridCell((sf::Vector2f(x, y)), drawables));
     }
-    return shapeMatrix;
+    x += 20;
+    y = 0;
+  }
+  return shapeMatrix;
 }
 
-std::array<int,2> findShapeFromMouse( sf::Vector2f mousePos ){
-    int xPos = int(mousePos.x) / 20;
-    int yPos = int(mousePos.y) / 20;
-    std::array<int, 2> arr = {xPos, yPos};
-    return arr;
+std::array<int, 2> findShapeFromMouse(sf::Vector2f mousePos) {
+  int xPos = int(mousePos.x) / 20;
+  int yPos = int(mousePos.y) / 20;
+  std::array<int, 2> arr = {xPos, yPos};
+  return arr;
 }
+
+sf::Vector2f Game::findShortestStep() {
+  int myXPos = 0;
+  int myYPos = 0;
+  int smallestValue = 999;
+  sf::Vector2f moveDirection = sf::Vector2f(0, 0);
+
+  sf::Vector2f monsterPosition = monster->getPosition();
+  myXPos = monsterPosition.x / 20;
+  myYPos = monsterPosition.y / 20;
+
+  // Check up
+  if ((myYPos - 1) >= 0 && grid[myYPos - 1][myXPos].value <= smallestValue) {
+    smallestValue = grid[myYPos - 1][myXPos].value;
+    moveDirection.x = 0;
+    moveDirection.y = -1;
+  }
+
+  // Check down
+  if ((myYPos + 1) < grid.size() &&
+      grid[myYPos + 1][myXPos].value <= smallestValue) {
+    smallestValue = grid[myYPos + 1][myXPos].value;
+    moveDirection.x = 0;
+    moveDirection.y = 1;
+  }
+
+  // Check left
+  if ((myXPos - 1) >= 0 && grid[myYPos][myXPos - 1].value <= smallestValue) {
+    smallestValue = grid[myYPos][myXPos - 1].value;
+    moveDirection.x = -1;
+    moveDirection.y = 0;
+  }
+
+  // Check right
+  if ((myXPos + 1) < grid[myYPos].size() &&
+      grid[myYPos][myXPos + 1].value <= smallestValue) {
+    smallestValue = grid[myYPos][myXPos + 1].value;
+    moveDirection.x = 1;
+    moveDirection.y = 0;
+  }
+
+  return moveDirection;
+};
+
+void Game::reversedBFSPathAlgorithm() {
+  std::queue<GridCell&> q;
+  int xPos = player->getPosition().x / 20;
+  int yPos = player->getPosition().y / 20;
+
+  GridCell& source = grid[yPos][xPos];
+  bool visited[grid.size()][grid[0].size()];
+
+  // Fill the visited array with "isWalkAble" bools
+  for (int y = 0; y < grid.size(); y++) {
+    for (int x = 0; x < grid[y].size(); x++) {
+      visited[y][x] = !grid[y][x].isWalkAble;
+    }
+  }
+
+  int xPosMonster = monster->getPosition().x / 20;
+  int yPosMonster = monster->getPosition().y / 20;
+  GridCell* sourceMonster = &grid[yPosMonster][xPosMonster];
+
+  q.push(source);
+  visited[yPosMonster][xPosMonster] = true;
+
+  while (!q.empty()) {
+    GridCell* p = &q.front();
+    q.pop();
+
+    if (p == sourceMonster) {
+      return;
+    }
+
+    // Check upper cell
+    if ((yPos - 1) >= 0 && visited[yPos - 1][xPos] == false) {
+      grid[yPos - 1][xPos].value = p->value + 1;
+      q.push(grid[yPos - 1][xPos]);
+      visited[yPos - 1][xPos] = true;
+    }
+
+    // Check lower cell
+    if ((yPos + 1) < grid.size() && visited[yPos + 1][xPos] == false) {
+      grid[yPos + 1][xPos].value = p->value + 1;
+      q.push(grid[yPos + 1][xPos]);
+      visited[yPos + 1][xPos] = true;
+    }
+
+    // Check left cell
+    if ((xPos - 1) >= 0 && visited[yPos][xPos - 1] == false) {
+      grid[yPos][xPos - 1].value = p->value + 1;
+      q.push(grid[yPos][xPos - 1]);
+      visited[yPos][xPos - 1] = true;
+    }
+
+    // Check right cell
+    if ((xPos + 1) < grid[yPos].size() && visited[yPos][xPos + 1] == false) {
+      grid[yPos][xPos + 1].value = p->value + 1;
+      q.push(grid[yPos][xPos + 1]);
+      visited[yPos][xPos + 1] = true;
+    }
+  }
+};
 
 void Game::run() {
-    // while (window.isOpen()) {
-    //     for (auto & action : playingActions) {
-    //         action();
-    //     }
+  // while (window.isOpen()) {
+  //     for (auto & action : playingActions) {
+  //         action();
+  //     }
 
-    //     window.clear();
-    //     for (auto & me : drawables) {
-    //         me->draw(window);
-    //     }
-    //     window.display();
+  //     window.clear();
+  //     for (auto & me : drawables) {
+  //         me->draw(window);
+  //     }
+  //     window.display();
 
-    //     if (player->checkWin()) {
-    //         window.close();
-    //     }
+  //     if (player->checkWin()) {
+  //         window.close();
+  //     }
 
-    //     sf::sleep(sf::milliseconds(20));
+  //     sf::sleep(sf::milliseconds(20));
 
-    //     sf::Event event;
-    //     while (window.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed) {
-    //             window.close();
-    //         }
-    //     }
-    // }
+  //     sf::Event event;
+  //     while (window.pollEvent(event)) {
+  //         if (event.type == sf::Event::Closed) {
+  //             window.close();
+  //         }
+  //     }
+  // }
 
-    while (window.isOpen()){
-        window.clear();
+  int pathFindCounter = 0;
 
-        // //Draw the grid.
-        for (auto & row : grid) {
-            for (auto & me : row) {
-                me.draw(window);
-            }
-        }
-        //Do the actions.
-        for(auto & action : editorActions){
-            action();
-        }
+  while (window.isOpen()) {
+    window.clear();
 
-        //Draw window.
-        window.display();
+    monster->move(findShortestStep());
+    pathFindCounter++;
 
-
-        sf::sleep(sf::milliseconds(20));
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
-                window.close();
-            }
-        }
+    if (pathFindCounter == 50) {
+      pathFindCounter = 0;
+      reversedBFSPathAlgorithm();
     }
 
-};
-
-sf::Vector2f Game::findShortestStep(){
-
-    int myXPos = 0;
-    int myYPos = 0;
-    int smallestValue = 999;
-    sf::Vector2f moveDirection = sf::Vector2f(0, 0);
-
-   sf::Vector2f monsterPosition = monster->getPosition();
-   myXPos = monsterPosition.x / 20;
-   myYPos = monsterPosition.y / 20;
-
-
-    // Check up
-    if ( (myYPos - 1) >= 0 && grid[myYPos - 1][myXPos].value <= smallestValue ){
-        smallestValue = grid[myYPos - 1][myXPos].value;
-        moveDirection.x = 0;
-        moveDirection.y = -1;
+    ////Draw the grid.
+    for (auto& row : grid) {
+      for (auto& me : row) {
+        me.draw(window);
+      }
     }
 
-    // Check down
-    if ( (myYPos + 1) < grid.size() && grid[myYPos + 1][myXPos].value <= smallestValue ){
-        smallestValue = grid[myYPos + 1][myXPos].value;
-        moveDirection.x = 0;
-        moveDirection.y = 1;
+    // Do the actions.
+    for (auto& action : editorActions) {
+      action();
     }
 
-    // Check left
-    if ( (myXPos - 1) >= 0 && grid[myYPos][myXPos - 1].value <= smallestValue ){
-        smallestValue = grid[myYPos][myXPos - 1].value;
-        moveDirection.x = -1;
-        moveDirection.y = 0;
+    // Draw window.
+    window.display();
+
+    sf::sleep(sf::milliseconds(20));
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
     }
-
-    // Check right
-    if ( (myXPos + 1) < grid[myYPos].size() && grid[myYPos][myXPos + 1].value <= smallestValue ){
-        smallestValue = grid[myYPos][myXPos + 1].value;
-        moveDirection.x = 1;
-        moveDirection.y = 0;
-    }
-
-    return moveDirection;
-
-};
-
-void Game::reversedBFSPathAlgorithm(){
-
-    std::queue<GridCell&> q;
-    int xPos = player->getPosition().x / 20;
-    int yPos = player->getPosition().y / 20;
-
-    GridCell & source = grid[yPos][xPos];
-    bool visited[grid.size()][grid[0].size()];
-
-    // Fill the visited array with "isWalkAble" bools
-    for( int y = 0; y < grid.size(); y++ ){
-        for( int x = 0; x < grid[y].size(); x++ ){   
-            visited[y][x] = ! grid[y][x].isWalkAble;
-        }
-    }
-
-    int xPosMonster = monster->getPosition().x / 20;
-    int yPosMonster = monster->getPosition().y / 20;
-    GridCell * sourceMonster = &grid[yPosMonster][xPosMonster];
-
-    q.push(source);
-    visited[yPosMonster][xPosMonster] = true;
-
-    while( ! q.empty() ){
-
-        GridCell *p = &q.front();
-        q.pop();
-
-        if( p == sourceMonster ){ return; }
-
-        // Check upper cell
-        if( (yPos - 1) >= 0 && visited[yPos - 1][xPos] == false ){
-            grid[ yPos - 1 ][xPos].value = p->value + 1;
-            q.push( grid[yPos - 1][xPos] );
-            visited[yPos - 1][xPos] = true;
-        }
-
-        // Check lower cell
-        if( (yPos + 1) < grid.size() && visited[yPos + 1][xPos] == false ){
-            grid[ yPos + 1 ][xPos].value = p->value + 1;
-            q.push( grid[yPos + 1][xPos] );
-            visited[yPos + 1][xPos] = true;
-        }
-
-        // Check left cell
-        if( (xPos - 1) >= 0 && visited[yPos][xPos - 1] == false ){
-            grid[ yPos][xPos - 1].value = p->value + 1;
-            q.push( grid[yPos][xPos - 1] );
-            visited[yPos][xPos - 1] = true;
-        }
-
-        // Check right cell
-        if( (xPos + 1) < grid[yPos].size() && visited[yPos][xPos + 1] == false ){
-            grid[ yPos][xPos + 1].value = p->value + 1;
-            q.push( grid[yPos][xPos + 1] );
-            visited[yPos][xPos + 1] = true;
-        }
-
-    }
-
-
-    
-
+  }
 };
