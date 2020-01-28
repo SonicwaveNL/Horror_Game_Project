0d3ef779610862a210fba27aad5fde2949d1500e
Vincent van Setten <vvampus@gmail.com>
2020-01-28 09:54:04 +0100

    monster never calling algorithm

--- Core/Sources/game.cpp
+++ Core/Sources/game.cpp
@@ -29,7 +29,7 @@ std::vector<std::vector<GridCell>> Game::createGrid(sf::Vector2u windowSize) {
     }
     shapeMatrix[30][30].setCellType(objectType::Player,
                                     &gameTextures[objectType::Player][0]);
-    shapeMatrix[20][20].setCellType(objectType::Monster,
+    shapeMatrix[PIXEL16][PIXEL16].setCellType(objectType::Monster,
                                     &gameTextures[objectType::Monster][0]);
     return shapeMatrix;
 }
@@ -120,115 +120,115 @@ void Game::loadSubVectors() {
 
 
 sf::Vector2f Game::findShortestStep() {
-    int64_t windowSurfaceSizeTImesTwo =
-        (window.getSize().x * window.getSize().y) * 2;
-    int myXPos = monster->getPosition().x / PIXEL16;
-    int myYPos = monster->getPosition().y / PIXEL16;
-    grid[myXPos][myYPos].value = windowSurfaceSizeTImesTwo;
-    int64_t smallestValue = windowSurfaceSizeTImesTwo;
-    sf::Vector2f moveDirection = sf::Vector2f(0, 0);
+  int64_t windowSurfaceSizeTImesTwo =
+      (window.getSize().x * window.getSize().y) * 2;
+  int myXPos = monster->getPosition().x / PIXEL16;
+  int myYPos = monster->getPosition().y / PIXEL16;
+  grid[myXPos][myYPos].value = windowSurfaceSizeTImesTwo;
+  int64_t smallestValue = windowSurfaceSizeTImesTwo;
+  sf::Vector2f moveDirection = sf::Vector2f(0, 0);
 
-    // check up
-    if ((myYPos - 1) >= 0 && grid[myXPos][myYPos - 1].value < smallestValue &&
-        grid[myXPos][myYPos - 1].visited) {
-        smallestValue = grid[myXPos][myYPos - 1].value;
-        moveDirection.x = 0;
-        moveDirection.y = -1;
-    }
-    // Check down
-    if ((myYPos + 1) < grid[myXPos].size() &&
-        grid[myXPos][myYPos + 1].value < smallestValue &&
-        grid[myXPos][myYPos + 1].visited) {
-        smallestValue = grid[myXPos][myYPos + 1].value;
-        moveDirection.x = 0;
-        moveDirection.y = 1;
-    }
-    // Check left
-    if ((myXPos - 1) >= 0 && grid[myXPos - 1][myYPos].value < smallestValue &&
-        grid[myXPos - 1][myYPos].visited) {
-        smallestValue = grid[myXPos - 1][myYPos].value;
-        moveDirection.x = -1;
-        moveDirection.y = 0;
-    }
-    // Check right
-    if ((myXPos + 1) < grid.size() &&
-        grid[myXPos + 1][myYPos].value < smallestValue &&
-        grid[myXPos + 1][myYPos].visited) {
-        smallestValue = grid[myXPos + 1][myYPos].value;
-        moveDirection.x = 1;
-        moveDirection.y = 0;
-    }
-    return moveDirection;
+  // check up
+  if ((myYPos - 1) >= 0 && grid[myXPos][myYPos - 1].value < smallestValue &&
+      grid[myXPos][myYPos - 1].visited) {
+    smallestValue = grid[myXPos][myYPos - 1].value;
+    moveDirection.x = 0;
+    moveDirection.y = -1;
+  }
+  // Check down
+  if ((myYPos + 1) < grid[myXPos].size() &&
+      grid[myXPos][myYPos + 1].value < smallestValue &&
+      grid[myXPos][myYPos + 1].visited) {
+    smallestValue = grid[myXPos][myYPos + 1].value;
+    moveDirection.x = 0;
+    moveDirection.y = 1;
+  }
+  // Check left
+  if ((myXPos - 1) >= 0 && grid[myXPos - 1][myYPos].value < smallestValue &&
+      grid[myXPos - 1][myYPos].visited) {
+    smallestValue = grid[myXPos - 1][myYPos].value;
+    moveDirection.x = -1;
+    moveDirection.y = 0;
+  }
+  // Check right
+  if ((myXPos + 1) < grid.size() &&
+      grid[myXPos + 1][myYPos].value < smallestValue &&
+      grid[myXPos + 1][myYPos].visited) {
+    smallestValue = grid[myXPos + 1][myYPos].value;
+    moveDirection.x = 1;
+    moveDirection.y = 0;
+  }
+  return moveDirection;
 }
 
 void Game::reversedBFSPathAlgorithm() {
-    int64_t windowSurfaceSizeTImesTwo =
-        (window.getSize().x * window.getSize().y) * 2;
+  int64_t windowSurfaceSizeTImesTwo =
+      (window.getSize().x * window.getSize().y) * 2;
 
-    for (auto & column : grid) {
-        for (auto & cell : column) {
-            if (cell.isWalkable()) {
-                cell.value = 0;
-                cell.visited = false;
-            } else {
-                cell.visited = true;
-                cell.value = windowSurfaceSizeTImesTwo;
-            }
-        }
+  for (auto &column : grid) {
+    for (auto &cell : column) {
+      if (cell.isWalkable()) {
+        cell.value = 0;
+        cell.visited = false;
+      } else {
+        cell.visited = true;
+        cell.value = windowSurfaceSizeTImesTwo;
+      }
+    }
+  }
+
+  std::queue<GridCell *> q;
+  int xPos = player->getPosition().x / PIXEL16;
+  int yPos = player->getPosition().y / PIXEL16;
+  GridCell *sourcePlayer = &grid[xPos][yPos];
+
+  int xPosMonster = monster->getPosition().x / PIXEL16;
+  int yPosMonster = monster->getPosition().y / PIXEL16;
+  GridCell *sourceMonster = &grid[xPosMonster][yPosMonster];
+  sourceMonster->value = windowSurfaceSizeTImesTwo;
+  sourceMonster->visited = true;
+
+  q.push(sourcePlayer);
+
+  while (!q.empty()) {
+    GridCell *p = q.front();
+    q.pop();
+    xPos = p->getPosition().x / PIXEL16;
+    yPos = p->getPosition().y / PIXEL16;
+
+    if (p == sourceMonster) {
+      return;
     }
 
-    std::queue<GridCell *> q;
-    int xPos = player->getPosition().x / 20;
-    int yPos = player->getPosition().y / 20;
-    GridCell * sourcePlayer = &grid[xPos][yPos];
-
-    int xPosMonster = monster->getPosition().x / 20;
-    int yPosMonster = monster->getPosition().y / 20;
-    GridCell * sourceMonster = &grid[xPosMonster][yPosMonster];
-    sourceMonster->value = windowSurfaceSizeTImesTwo;
-    sourceMonster->visited = true;
-
-    q.push(sourcePlayer);
-
-    while (!q.empty()) {
-        GridCell * p = q.front();
-        q.pop();
-        xPos = p->getPosition().x / PIXEL16;
-        yPos = p->getPosition().y / PIXEL16;
-
-        if (p == sourceMonster) {
-            return;
-        }
-
-        // set upper cell
-        if ((yPos - 1) >= 0 && grid[xPos][yPos - 1].visited == false) {
-            grid[xPos][yPos - 1].value = p->value + 1;
-            q.push(&grid[xPos][yPos - 1]);
-            grid[xPos][yPos - 1].visited = true;
-        }
-
-        // set lower cell
-        if ((yPos + 1) < grid[xPos].size() &&
-            grid[xPos][yPos + 1].visited == false) {
-            grid[xPos][yPos + 1].value = p->value + 1;
-            q.push(&grid[xPos][yPos + 1]);
-            grid[xPos][yPos + 1].visited = true;
-        }
-
-        // set left cell
-        if ((xPos - 1) >= 0 && grid[xPos - 1][yPos].visited == false) {
-            grid[xPos - 1][yPos].value = p->value + 1;
-            q.push(&grid[xPos - 1][yPos]);
-            grid[xPos - 1][yPos].visited = true;
-        }
-
-        // set right cell
-        if ((xPos + 1) < grid.size() && grid[xPos + 1][yPos].visited == false) {
-            grid[xPos + 1][yPos].value = p->value + 1;
-            q.push(&grid[xPos + 1][yPos]);
-            grid[xPos + 1][yPos].visited = true;
-        }
+    // set upper cell
+    if ((yPos - 1) >= 0 && grid[xPos][yPos - 1].visited == false) {
+      grid[xPos][yPos - 1].value = p->value + 1;
+      q.push(&grid[xPos][yPos - 1]);
+      grid[xPos][yPos - 1].visited = true;
     }
+
+    // set lower cell
+    if ((yPos + 1) < grid[xPos].size() &&
+        grid[xPos][yPos + 1].visited == false) {
+      grid[xPos][yPos + 1].value = p->value + 1;
+      q.push(&grid[xPos][yPos + 1]);
+      grid[xPos][yPos + 1].visited = true;
+    }
+
+    // set left cell
+    if ((xPos - 1) >= 0 && grid[xPos - 1][yPos].visited == false) {
+      grid[xPos - 1][yPos].value = p->value + 1;
+      q.push(&grid[xPos - 1][yPos]);
+      grid[xPos - 1][yPos].visited = true;
+    }
+
+    // set right cell
+    if ((xPos + 1) < grid.size() && grid[xPos + 1][yPos].visited == false) {
+      grid[xPos + 1][yPos].value = p->value + 1;
+      q.push(&grid[xPos + 1][yPos]);
+      grid[xPos + 1][yPos].visited = true;
+    }
+  }
 };
 std::unordered_map<objectType, std::vector<sf::Texture>>
 Game::loadTextures(std::string file, sf::Image & source) {
@@ -255,214 +255,7 @@ Game::loadTextures(std::string file, sf::Image & source) {
     return tmpCont;
 };
 
-void Game::markGridCellsToDraw() {
-    int viewDistance = 10;
-    for (auto & column : grid) {
-        for (auto & item : column) {
-            item.ableToDraw = false;
-            item.behindWall = false;
-            item.drawValue = 0;
-        }
-    }
-    std::queue<GridCell *> q;
-    int xPos = player->getPosition().x / PIXEL16;
-    int yPos = player->getPosition().y / PIXEL16;
-    GridCell * sourcePlayer = &grid[xPos][yPos];
-    sourcePlayer->ableToDraw = true;
-    sourcePlayer->drawValue = 0;
-    q.push(sourcePlayer);
 
-    while (!q.empty()) {
-        GridCell * p = q.front();
-        q.pop();
-        xPos = p->getPosition().x / PIXEL16;
-        yPos = p->getPosition().y / PIXEL16;
-        // set upper cell
-        if (p->drawValue < viewDistance) {
-            if ((yPos - 1) >= 0 && grid[xPos][yPos - 1].ableToDraw == false) {
-                grid[xPos][yPos - 1].drawValue = p->drawValue + 1;
-                q.push(&grid[xPos][yPos - 1]);
-                grid[xPos][yPos - 1].ableToDraw = true;
-            }
-
-            // set lower cell
-            if ((yPos + 1) < grid[xPos].size() &&
-                grid[xPos][yPos + 1].ableToDraw == false) {
-                grid[xPos][yPos + 1].drawValue = p->drawValue + 1;
-                q.push(&grid[xPos][yPos + 1]);
-                grid[xPos][yPos + 1].ableToDraw = true;
-            }
-
-            // set left cell
-            if ((xPos - 1) >= 0 && grid[xPos - 1][yPos].ableToDraw == false) {
-                grid[xPos - 1][yPos].drawValue = p->drawValue + 1;
-                q.push(&grid[xPos - 1][yPos]);
-                grid[xPos - 1][yPos].ableToDraw = true;
-            }
-
-            // set right cell
-            if ((xPos + 1) < grid.size() &&
-                grid[xPos + 1][yPos].ableToDraw == false) {
-                grid[xPos + 1][yPos].drawValue = p->drawValue + 1;
-                q.push(&grid[xPos + 1][yPos]);
-                grid[xPos + 1][yPos].ableToDraw = true;
-            }
-        }
-        // SOORT NIET DOOR MUREN KIJKEN SHIT
-        // if (p->drawValue < viewDistance) {
-        //   // set uppercell
-        //   if ((yPos - 1) >= 0) {
-        //     grid[xPos][yPos - 1].drawValue = p->drawValue + 1;
-        //     if (grid[xPos][yPos - 1].behindWall == false) {
-        //       q.push(&grid[xPos][yPos - 1]);
-        //       grid[xPos][yPos - 1].ableToDraw = true;
-        //     }
-        //     if (p->getCellType() == objectType::Wall) {
-        //       for (int i = 1; i < viewDistance; i++) {
-        //         if (yPos - i >= 0) {
-        //           grid[xPos][yPos - i].behindWall = true;
-        //         }
-        //       }
-        //     }
-        //   }
-        //   // set lower cell
-        //   if ((yPos + 1) < grid[xPos].size()) {
-        //     if (grid[xPos][yPos + 1].behindWall == false) {
-        //       q.push(&grid[xPos][yPos + 1]);
-        //       grid[xPos][yPos + 1].ableToDraw = true;
-        //     }
-        //     grid[xPos][yPos + 1].drawValue = p->drawValue + 1;
-        //     if (p->getCellType() == objectType::Wall) {
-        //       for (int i = 1; i < viewDistance; i++) {
-        //         if (yPos + i < grid[xPos].size()) {
-        //           grid[xPos][yPos + i].behindWall = true;
-        //         }
-        //       }
-        //     }
-        //   }
-
-        //   // set left cell
-        //   if ((xPos - 1) >= 0) {
-        //     grid[xPos - 1][yPos].drawValue = p->drawValue + 1;
-        //     if (grid[xPos - 1][yPos].behindWall == false) {
-        //       q.push(&grid[xPos - 1][yPos]);
-        //       grid[xPos - 1][yPos].ableToDraw = true;
-        //     }
-        //     if (p->getCellType() == objectType::Wall) {
-        //       for (int i = 1; i < viewDistance; i++) {
-        //         if (xPos - i >= 0) {
-        //           grid[xPos - i][yPos].behindWall = true;
-        //         }
-        //       }
-        //     }
-        //   }
-
-        //   // set right cell
-        //   if ((xPos + 1) < grid.size()) {
-        //     grid[xPos + 1][yPos].drawValue = p->drawValue + 1;
-        //     q.push(&grid[xPos + 1][yPos]);
-        //     if (grid[xPos + 1][yPos].behindWall == false) {
-        //       grid[xPos + 1][yPos].ableToDraw = true;
-        //     }
-        //     if (p->getCellType() == objectType::Wall) {
-        //       for (int i = 1; i < viewDistance; i++) {
-        //         if (xPos + i < grid.size()) {
-        //           grid[xPos + i][yPos].behindWall = true;
-        //         }
-        //       }
-        //     }
-        //   }
-        // }
-
-        // RARE SHIT
-        // //set upper cell
-        // for (int i = 1; i < viewDistance; i++) {
-        //   if ((yPos - i) >= 0 && grid[xPos][yPos - i].ableToDraw == false &&
-        //       grid[xPos][yPos - i].behindWall == false) {
-        //     if (grid[xPos][yPos - i].getType() == objectType::Wall) {
-        //       for (int a = i + 1; a < viewDistance; a++) {
-        //         if (yPos - a >= 0) {
-        //           grid[xPos][yPos - a].behindWall = true;
-        //           grid[xPos][yPos - a].drawValue = p->drawValue + 1;
-        //           grid[xPos][yPos -a].ableToDraw = false;
-        //         }
-        //       }
-        //       break;
-        //     }
-        //     grid[xPos][yPos - i].drawValue = p->drawValue + 1;
-        //     if (grid[xPos][yPos - i].drawValue <= viewDistance) {
-        //       q.push(&grid[xPos][yPos - i]);
-        //       grid[xPos][yPos - i].ableToDraw = true;
-        //     }
-        //   }
-        // }
-
-        // set lower cell
-        // for (int i = 1; i < viewDistance; i++) {
-        //   if ((yPos + i) < grid[xPos].size() &&
-        //       grid[xPos][yPos + i].ableToDraw == false &&
-        //       grid[xPos][yPos + i].behindWall == false &&
-        //       grid[xPos][yPos + i].drawValue <= viewDistance) {
-        //     if (grid[xPos][yPos + i].getType() == objectType::Wall) {
-        //       for (int a = i + 1; a < viewDistance; a++) {
-        //         if (yPos + a < grid[xPos].size()) {
-        //           grid[xPos][yPos + a].behindWall = true;
-        //         }
-        //       }
-        //       break;
-        //     }
-        //     grid[xPos][yPos + i].drawValue = p->drawValue + 1;
-        //     if (grid[xPos][yPos + i].drawValue <= viewDistance) {
-        //       q.push(&grid[xPos][yPos + i]);
-        //       grid[xPos][yPos + i].ableToDraw = true;
-        //     }
-        //   }
-        // }
-
-        // set left cell
-        // for (int i = 1; i < viewDistance; i++) {
-        //   if ((xPos - i) >= 0 && grid[xPos - i][yPos].ableToDraw == false &&
-        //       grid[xPos - i][yPos].behindWall == false &&
-        //       grid[xPos - i][yPos].drawValue <= viewDistance) {
-        //     if (grid[xPos - i][yPos].getType() == objectType::Wall) {
-        //       for (int a = i + 1; a < viewDistance; a++) {
-        //         if (xPos - a >= 0) {
-        //           grid[xPos - a][yPos].behindWall = true;
-        //         }
-        //       }
-        //       break;
-        //     }
-        //     grid[xPos - i][yPos].drawValue = p->drawValue + 1;
-        //     if (grid[xPos - i][yPos].drawValue <= viewDistance) {
-        //       q.push(&grid[xPos - i][yPos]);
-        //       grid[xPos - i][yPos].ableToDraw = true;
-        //     }
-        //   }
-        // }
-
-        // set right cell
-        // for (int i = 1; i < viewDistance; i++) {
-        //   if ((xPos + i) < grid.size() &&
-        //       grid[xPos + i][yPos].ableToDraw == false &&
-        //       grid[xPos + i][yPos].behindWall == false &&
-        //       grid[xPos + i][yPos].drawValue <= viewDistance) {
-        //     if (grid[xPos + i][yPos].getType() == objectType::Wall) {
-        //       for (int a = i; a < viewDistance; a++) {
-        //         if (xPos + a < grid.size()) {
-        //           grid[xPos + a][yPos].behindWall = true;
-        //         }
-        //       }
-        //       break;
-        //     }
-        //     grid[xPos + i][yPos].drawValue = p->drawValue + 1;
-        //     if (grid[xPos + i][yPos].drawValue <= viewDistance) {
-        //       q.push(&grid[xPos + i][yPos]);
-        //       grid[xPos + i][yPos].ableToDraw = true;
-        //     }
-        //   }
-        // }
-    }
-}
 
 void Game::draw(std::vector<std::shared_ptr<IObject>> & drawables) {
     for (std::shared_ptr<IObject> drawable : drawables) {
@@ -571,11 +364,12 @@ void Game::run() {
                     int myXPos = (monsterPosition.x) / PIXEL16;
                     int myYPos = (monsterPosition.y) / PIXEL16;
 
-                    // bool onValidWall = false;
                     if (monster->getPosition() ==
                         grid[myXPos][myYPos].getPosition()) {
                         monster->moveIfPossible(findShortestStep());
                     } else {
+                      std::cout << "monster pos: " << monster->getPosition().x << ", " << monster->getPosition().y << std::endl;
+                      std::cout << "gridPos: " <<  grid[myXPos][myYPos].getPosition().x << ", " << grid[myXPos][myYPos].getPosition().y << std::endl;
                         monster->moveOld();
                     }
                 }
@@ -619,7 +413,7 @@ void Game::run() {
                 break;
         }
         window.display();
-        sf::sleep(sf::milliseconds(20));
+        sf::sleep(sf::milliseconds(PIXEL16));
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
